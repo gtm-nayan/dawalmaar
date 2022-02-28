d966dd3f224b2424e024c21d0896b5d363457e2d
gtm-nayan <gtmnayan@gmail.com>
2022-02-28 17:28:30 +0545

    Ceremony

---
+++ bot/src/game.rs
@@ -0,0 +1,31 @@
+// use dawalmaar::{
+// 	cards::{Card, Rank, Suit},
+// 	game::Game as IGame,
+// };
+
+// struct Game {
+// 	i_game: IGame,
+// 	players_map: Vec<usize>,
+// }
+
+// impl Game {
+// 	pub fn new() -> Self {
+// 		Self {
+// 			i_game: IGame::new(),
+// 			players_map: vec![],
+// 		}
+// 	}
+// }
+
+// impl Game {
+// 	pub fn add_player(&mut self){}
+
+// 	pub fn get_hand(&mut self) {
+// 	}
+
+// 	pub fn play_card(&mut self){
+
+// 	}
+
+// 	pub fn start(&mut self){}
+// }
--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,11 +1,51 @@
-use dotenv::dotenv;
-use std::env;
+mod game;
 
-fn main() {
+use dotenv::dotenv;
+use poise::{
+	serenity_prelude::{CreateAllowedMentions, UserId},
+	FrameworkOptions, PrefixFrameworkOptions,
+};
+use std::{
+	collections::{HashMap, HashSet},
+	env,
+	sync::Mutex,
+};
+
+pub struct Data {
+	games: Mutex<HashMap<String, u32>>,
+}
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[tokio::main]
+async fn main() {
 	dotenv().ok();
-	println!("{}", get_token());
+
+	let prefix_options: PrefixFrameworkOptions<Data, Error> = PrefixFrameworkOptions {
+		prefix: Some("cap".into()),
+		additional_prefixes: vec![],
+		dynamic_prefix: None,
+		stripped_dynamic_prefix: None,
+		mention_as_prefix: true,
+		case_insensitive_commands: false,
+		edit_tracker: None,
+		execute_self_messages: false,
+		execute_untracked_edits: false,
+		ignore_edits_if_not_yet_responded: true,
+	};
+
+	let framework_options: FrameworkOptions<Data, Error> = FrameworkOptions {
+		
+		pre_command: |ctx| {
+			Box::pin(async move {
+				println!("{:?}", ctx.data().games.lock());
+			})
+		},
+		..Default::default()
+	};
 }
 
 fn get_token() -> String {
-	env::var("DISCORD_TOKEN").expect("Login token was not provided.")
+	env::var("DISCORD_TOKEN").expect("Token not provided.")
 }
